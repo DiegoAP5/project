4b55d27c0386baf9e7cd7ca6c878fc58a2e17a27
Grnting <diegoalv.n@gmail.com>
2022-08-16 23:42:35 -0500

    Project

---
+++ app.js
@@ -0,0 +1,343 @@
+//invocar express 
+const express = require('express');
+const app = express();
+
+//setear urlencoded para datos de formulario
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+//invocar dotenv
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './env/.env'
+});
+
+
+//directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+//establecer motor de plantillas
+app.set('view engine', 'ejs');
+
+//invocar bcryptjs
+const bcryptjs = require('bcryptjs');
+
+
+
+//var de session
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialzed: true
+}));
+
+
+
+//Invocar modulo de base de datos
+const connection = require('./database/db');
+
+
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/auth', (req, res) => {
+    const contrasena = req.body.contrasena;
+    const usuario = req.body.usuario;
+
+    if (usuario && contrasena) {
+        connection.query('SELECT * FROM administradores WHERE usuario = ? AND contrasena = ?', [usuario, contrasena], (error, results) => {
+
+            if (results.length == 0) {
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "La cuenta no existe",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta: 'login'
+                });
+            } else {
+                req.session.loggedin = true;
+                req.session.usuario = results[0].usuario;
+                req.session.administrador = results[0].administrador;
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Ingresando...",
+                    alertMessage: "Entrando a la cuenta",
+                    alertIcon: "success",
+                    showConfirmButton: false,
+                    timer: 1500,
+                    ruta: ''
+                });
+            }
+
+        })
+    }
+
+})
+
+app.post('/adduser', (req, res) => {
+    const usuario = req.body.nombre;
+    const contrasena = req.body.contrasena;
+    const check = req.body.check;
+    if (check != 'si') {
+        const check = "no";
+    }
+
+    if (usuario && contrasena) {
+        connection.query('INSERT INTO administradores (usuario, contrasena, administrador) VALUES (?, ?, ?);', [usuario, contrasena, check], (error, results) => {
+
+            connection.query('Select * from administradores ORDER BY administradores.id ASC;', (error, results) => {
+
+                res.render('administrador', {
+                    alert: true,
+                    alertTitle: "Correcto",
+                    alertMessage: "Usuario agregado",
+                    alertIcon: "success",
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta: 'administrador',
+                    usuarios: results,
+                    insession: req.session.usuario
+                });
+            })
+        })
+    }
+})
+
+/*app.get('/agregar-dueno', (req,res)=>{
+    if (req.session.loggedin) {
+        res.render('agregar-dueno', {
+            login: true
+        });
+    }
+    else {
+        res.render('login', {
+            login: false
+        })
+    }
+})*/
+
+/*app.post('/addclient',(req,res)=>{
+    const nombre = req.body.nombre
+    const id_mascota = req.session.id_mascota
+    const id_cliente = req.session.id_cliente
+    const telefono = req.body.telefono
+    const direccion = req.body.direccion
+    if(telefono && direccion){
+        connection.query('INSERT INTO clientes (id_cliente, nombre, telefono, direccion, id_mascota) VALUES (?,?,?,?,?)',
+        [id_cliente,nombre,telefono,direccion,id_mascota],(err,results)=>{
+            res.render('agregar-dueno',{
+                alert: true,
+                    alertTitle: "Agregado",
+                    alertMessage: "Dueño agregado correctamente",
+                    alertIcon: "success",
+                    showConfirmButton: false,
+                    timer: 2500,
+                    ruta: 'agregar'
+            })
+        })
+    }
+})
+*/
+app.get('/agregar', (req, res) => {
+    if (req.session.loggedin) {
+        res.render('agregar', {
+            login: true
+        });
+    }
+    else {
+        res.render('login', {
+            login: false
+        })
+    }
+})
+
+app.post('/addpet', (req, res) => {
+    console.log('entro')
+    const especie = req.body.especie
+    const raza = req.body.raza
+    const mnombre = req.body.mnombre
+    const edad = req.body.edad
+    const adicional = req.body.adicional
+    const fecha_entrada = req.body.entrada
+    const hora = req.body.hora
+    const fecha_salida = req.body.salida
+    const dnombre = req.body.dnombre
+    const telefono = req.body.telefono
+    const direccion = req.body.direccion
+
+    if (dnombre) {
+        connection.query('INSERT INTO clientes (nombre, telefono, direccion) VALUES (?, ?, ?);', [dnombre, telefono, direccion], (err, results) => {
+            console.log('cliente')
+            connection.query('Select * FROM clientes ORDER BY id_cliente DESC;', (err, results) => {
+                id_cliente = results[0].id_cliente;
+                console.log(id_cliente)
+
+                console.log('** datos **')
+                console.log(especie)
+                console.log(raza)
+                console.log(edad)
+                console.log(mnombre)
+                console.log(adicional)
+                console.log(id_cliente)
+                console.log(dnombre)
+                console.log(fecha_entrada)
+                console.log(hora)
+                console.log(fecha_salida)
+
+                connection.query('INSERT INTO mascotas (especie, raza, edad, nombre, informacion_adicional, nombre_cliente, id_cliente, fecha_entrada, hora, fecha_salida) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', 
+                [especie, raza, edad, mnombre, adicional, dnombre, id_cliente, fecha_entrada, hora, fecha_salida], (err, results) => {
+                        res.render('agregar', {
+                            alert: true,
+                            alertTitle: "Agregado",
+                            alertMessage: "Mascota agregada correctamente",
+                            alertIcon: "success",
+                            showConfirmButton: false,
+                            timer: 2000,
+                            ruta: 'agregar'
+                        })
+                    })
+            })
+        })
+    }
+})
+
+app.get('/ver', (req, res) => {
+    if (req.session.loggedin) {
+        connection.query('SELECT * FROM mascotas;', (err,results)=>{
+            res.render('ver', {
+            login: true,
+            mascotas: results,
+            insession: req.session.usuario
+        });
+        })
+    }
+    else {
+        res.render('login', {
+            login: false
+        })
+    }
+})
+
+app.post('/eliminar', (req, res) => {
+    if (req.session.loggedin) {
+        res.render('eliminar', {
+            login: true
+        });
+    }
+    else {
+        res.render('login', {
+            login: false
+        })
+    }
+})
+
+app.get('/administrador', (req, res) => {
+    if (req.session.loggedin) {
+        if (req.session.administrador == 'si') {
+
+            connection.query('Select * from administradores ORDER BY administradores.id ASC;', (error, results) => {
+                res.render('administrador', {
+                    usuarios: results,
+                    insession: req.session.usuario
+                })
+            })
+
+
+        }
+        else {
+            res.render('index', {
+                alertadmin: 'true',
+                alertMessage: 'Se necesitan permisos de admin',
+                ruta: ''
+            })
+        }
+    }
+    else {
+        res.render('login', {
+            login: false
+        })
+    }
+})
+
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/registro', async (req, res) => {
+    const usuario = req.body.usuario;
+    const contrasena = req.body.contrasena;
+    if (nombre && contrasena) {
+        connection.query('INSERT INTO administradores (usuario,contrasena) VALUES (?,?)', [usuario, contrasena], (req, results) => {
+            res.render('administrador', {
+                alert: true,
+                alertTitle: "Registro",
+                alertMessage: "Registro exitoso",
+                alertIcon: "success",
+                showConfirmButton: true,
+                timer: false,
+                ruta: 'administrador'
+            })
+        })
+    }
+    else {
+        res.render('registro', {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Por favor, ingrese datos",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: 'administrador'
+        })
+    }
+})
+
+
+
+//autenticar paginas
+app.get('/', (req, res) => {
+
+
+
+    if (req.session.loggedin) {
+
+        root = req.session.administrador;
+        if (root == "si") {
+            root = true;
+        }
+
+        res.render('index', {
+            login: true,
+            root: root
+        });
+    }
+    else {
+        res.render('login', {
+            login: false
+
+        })
+    }
+
+})
+
+//salir de la cuenta (logout)
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
+})
+
+app.listen(3000, (req, res) => {
+    console.log('--Server running in port 3000--');
+})
---
+++ veterinaria.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-07-2022 a las 02:13:57
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `veterinaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administradores`
+--
+CREATE TABLE `administradores` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `contrasena` varchar(4) NOT NULL,
+  `administrador` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `administradores`
+--
+
+INSERT INTO `administradores` (`id`, `usuario`, `contrasena`, `administrador`) VALUES
+(0, 'root', '9043', 'si'),
+(1, 'alfonso', '3242', 'no'),
+(2, 'diego', '1234', 'si'),
+(3, 'javier', '1234', 'si'),
+(16, 'darinel', '1234', 'si');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `direccion` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `telefono`, `direccion`) VALUES
+(20, 'Leonardo', '9612428401', 'Lado oriente'),
+(21, 'Javier', '9615440033', 'Lado sur'),
+(22, 'Diego Perez', '9616362344', 'Lado Norte');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `id_mascota` int(11) NOT NULL,
+  `especie` varchar(20) NOT NULL,
+  `raza` varchar(20) NOT NULL,
+  `edad` varchar(80) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `informacion_adicional` varchar(120) NOT NULL,
+  `nombre_cliente` varchar(80) NOT NULL,
+  `id_cliente` int(10) NOT NULL,
+  `fecha_entrada` date NOT NULL,
+  `hora` time NOT NULL,
+  `fecha_salida` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `mascotas`
+--
+
+INSERT INTO `mascotas` (`id_mascota`, `especie`, `raza`, `edad`, `nombre`, `informacion_adicional`, `nombre_cliente`, `id_cliente`, `fecha_entrada`, `hora`, `fecha_salida`) VALUES
+(20, 'Perro', 'Husky', '5 meses', 'Laila', 'Lomo cafe con negro', 'Leonardo', 20, '2022-07-19', '19:08:00', '2022-07-20'),
+(21, 'Gato', 'Angora', '8 meses', 'Cati', 'Pata derecha blanca', 'Javier', 21, '2022-07-19', '19:11:00', '2022-07-20'),
+(22, 'Hamster', 'No aplica', '1 año', 'Cacho', 'Color cafe oscuro', 'Diego Perez', 22, '2022-07-19', '19:12:00', '2022-07-21');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`id_mascota`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
